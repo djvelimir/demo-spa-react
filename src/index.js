27490f2bcedadfbdbe3094d2a55479988df1d73a
Velimir Đurković <velimir.djurkovic@gmail.com>
2024-05-14 22:42:43 +0200

    Add components, views and routing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
+import PageNotFoundView from './views/PageNotFoundView.js';
+import HomeView from './views/HomeView.js';
+import AboutView from './views/AboutView.js';
+import ContactView from './views/ContactView.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            {/* <Route index element={<HomeView />} /> */}
+            <Route path="home" element={<HomeView />} />
+            <Route path="about" element={<AboutView />} />
+            <Route path="contact" element={<ContactView />} />
+            <Route exact path="/"  element={<Navigate to="/home" replace />} />
+            <Route path="*" element={<PageNotFoundView />} />
+          </Route>
+        </Routes>
+      </HashRouter>
   </React.StrictMode>
 );
 
