e2a48887ce5eb86a525ef30d39689268620ce4de
Velimir Đurković <velimir.djurkovic@gmail.com>
2024-05-15 22:07:40 +0200

    Added tailwindcss

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -13,32 +13,32 @@ function HomeView() {
           <div className="xl:w-1/4 md:w-1/2 p-4">
             <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
               <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content"></img>
-              <h3 className="tracking-widest text-green-400 text-xs font-medium title-font">SUBTITLE</h3>
-              <h2 className="text-lg text-white font-medium title-font mb-4">Chichen Itza</h2>
+              <p className="tracking-widest text-green-400 text-xs font-medium title-font">SUBTITLE</p>
+              <p className="text-lg text-white font-medium title-font mb-4">Chichen Itza</p>
               <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
             </div>
           </div>
           <div className="xl:w-1/4 md:w-1/2 p-4">
             <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
               <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content"></img>
-              <h3 className="tracking-widest text-green-400 text-xs font-medium title-font">SUBTITLE</h3>
-              <h2 className="text-lg text-white font-medium title-font mb-4">Colosseum Roma</h2>
+              <p className="tracking-widest text-green-400 text-xs font-medium title-font">SUBTITLE</p>
+              <p className="text-lg text-white font-medium title-font mb-4">Colosseum Roma</p>
               <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
             </div>
           </div>
           <div className="xl:w-1/4 md:w-1/2 p-4">
             <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
               <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content"></img>
-              <h3 className="tracking-widest text-green-400 text-xs font-medium title-font">SUBTITLE</h3>
-              <h2 className="text-lg text-white font-medium title-font mb-4">Great Pyramid of Giza</h2>
+              <p className="tracking-widest text-green-400 text-xs font-medium title-font">SUBTITLE</p>
+              <p className="text-lg text-white font-medium title-font mb-4">Great Pyramid of Giza</p>
               <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
             </div>
           </div>
           <div className="xl:w-1/4 md:w-1/2 p-4">
             <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
               <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content"></img>
-              <h3 className="tracking-widest text-green-400 text-xs font-medium title-font">SUBTITLE</h3>
-              <h2 className="text-lg text-white font-medium title-font mb-4">San Francisco</h2>
+              <p className="tracking-widest text-green-400 text-xs font-medium title-font">SUBTITLE</p>
+              <p className="text-lg text-white font-medium title-font mb-4">San Francisco</p>
               <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
             </div>
           </div>
